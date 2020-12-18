7b5f91be7412724d97ab0f0546bc3ebdd1372d79
prabhat1432 <prabhat.malhotra@thoughtworks.com>
2020-12-18 12:48:25 +0530

    creating s3 bucket and vpc

---
+++ session_3/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-2"
+  profile = "malhotra-personal"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "malhotra-tf-test"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name = "tf test"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_default_vpc" "tf-default-vpc" {}
